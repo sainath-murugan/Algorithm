f0c43af0d9e4d92c59f6ac5e67c3bfbd8c3fd1ee
sainath-murugan <saimu.sn79@gmail.com>
2021-08-14 22:20:04 +0530

    admin url fixed and modified requirements.txt

--- webapp/urls.py
+++ webapp/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/algorithm/sainath-12345/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('algorithm_1.urls')),
     path('accounts/', include('user.urls')),
